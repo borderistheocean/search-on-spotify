f58ab024a8f1a089abcb303619b80d86f9ed946b
borderistheocean <68243063+borderistheocean@users.noreply.github.com>
2024-04-13 18:44:57 +0100

    Initial creation of extension, messaging implemented to get around V3 limitations around using the window API by background scripts

---
+++ background.js
@@ -0,0 +1,21 @@
+chrome.contextMenus.create({
+  id: "search-selection",
+  title: "Search Spotify for \"%s\"",
+  contexts: ["selection"]
+});
+
+// # TO DO: HANDLE HYPERLINK SEARCHES
+
+chrome.contextMenus.onClicked.addListener(function (info) {
+  if (info.menuItemId == "search-selection") {
+    function logTabs(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { search: encodeURIComponent(info.selectionText) });
+    }
+    function onError(error) {
+      console.error(`Error: ${error}`);
+    }
+    chrome.tabs
+      .query({ currentWindow: true, active: true })
+      .then(logTabs, onError);
+  }
+});
